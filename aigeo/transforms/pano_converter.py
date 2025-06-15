fef4097a653cd07cbad882a2ec3a81db5e56481d
mozhaa <mozhay2005@gmail.com>
2025-06-15 19:38:48 +0300

    pano converter implementation outside of class

--- aigeo/transforms/pano_converter.py
+++ aigeo/transforms/pano_converter.py
@@ -5,57 +5,68 @@ import torch
 from torch.nn import functional as F
 
 
+def get_cube_center(phi: float, theta: float) -> np.ndarray:
+    return np.array([np.cos(phi) * np.cos(theta), np.sin(phi) * np.cos(theta), -np.sin(theta)])
+
+
+def calculate_support_vectors(phi: float, theta: float, fov: float) -> Tuple[np.ndarray, np.ndarray]:
+    v = get_cube_center(phi, theta)
+    s = np.tan(fov / 2)
+
+    up = np.array([0, 0, 1])
+    if np.allclose(v, up):
+        up = np.array([1, 0, 0])
+
+    dj = -np.cross(v, up)
+    di = np.cross(v, dj)
+
+    dj *= s / np.linalg.norm(dj)
+    di *= s / np.linalg.norm(di)
+
+    return di, dj
+
+
+def cube_to_3d(i: float, j: float, di: np.ndarray, dj: np.ndarray, v: np.ndarray) -> np.ndarray:
+    return v + di * (2 * i - 1) + dj * (2 * j - 1)
+
+
+def cube_to_pano(i: float, j: float, di: np.ndarray, dj: np.ndarray, v: np.ndarray) -> Tuple[float, float]:
+    x, y, z = cube_to_3d(i, j, di, dj, v)
+    phi = np.atan2(y, x)
+    rsin = np.hypot(x, y)
+    theta = np.atan2(z, rsin)
+    ox = 2 * theta / np.pi
+    oy = phi / np.pi
+    return ox, oy
+
+
+def prepare_base_mapping(size: int, phi: float, theta: float, fov: float, batch_size: int, device: str) -> torch.Tensor:
+    mapping = torch.zeros((batch_size, size, size, 2), dtype=torch.float, device=device)
+    di, dj = calculate_support_vectors(phi, theta, fov)
+    v = get_cube_center(phi, theta)
+    for i in range(size):
+        for j in range(size):
+            xmap, ymap = cube_to_pano((i + 0.5) / size, (j + 0.5) / size, di, dj, v)
+            mapping[:, i, j, 0] = ymap
+            mapping[:, i, j, 1] = xmap
+    return mapping
+
+
 class PanoConverter:
     def __init__(
-        self, size: int, phi: float, theta: float, fov: float = np.pi / 2, batch_size: int = 1, device: Any = "cpu"
+        self,
+        size: int,
+        phi: float,
+        theta: float,
+        fov: float,
+        batch_size: int,
+        device: Any = "cpu",
     ) -> None:
-        self.size = size
-        self.phi = phi
-        self.theta = theta
-        self.fov = fov
-        self.batch_size = batch_size
         self.device = device
+        self.batch_size = batch_size
 
-        self._calculate_support_vectors()
-        self._prepare_base_mapping()
-
-    def _calculate_support_vectors(self) -> None:
-        self.v = np.array(
-            [np.cos(self.phi) * np.cos(self.theta), np.sin(self.phi) * np.cos(self.theta), -np.sin(self.theta)]
-        )
-        s = np.tan(self.fov / 2)
-
-        up = np.array([0, 0, 1])
-        if np.allclose(self.v, up):
-            up = np.array([1, 0, 0])
-
-        self.dj = -np.cross(self.v, up)
-        self.di = np.cross(self.v, self.dj)
-
-        self.dj *= s / np.linalg.norm(self.dj)
-        self.di *= s / np.linalg.norm(self.di)
-
-    def _cube_to_3d(self, i: float, j: float) -> np.ndarray:
-        return self.v + self.di * (2 * i - 1) + self.dj * (2 * j - 1)
-
-    def _cube_to_pano(self, i: float, j: float) -> Tuple[float, float]:
-        x, y, z = self._cube_to_3d(i, j)
-        phi = np.atan2(y, x)
-        rsin = np.hypot(x, y)
-        theta = np.atan2(z, rsin)
-        ox = 2 * theta / np.pi
-        oy = phi / np.pi
-        return ox, oy
-
-    @torch.no_grad()
-    def _prepare_base_mapping(self) -> None:
-        self.base_mapping = torch.zeros((self.batch_size, self.size, self.size, 2), dtype=torch.float)
-        for i in range(self.size):
-            for j in range(self.size):
-                xmap, ymap = self._cube_to_pano((i + 0.5) / self.size, (j + 0.5) / self.size)
-                self.base_mapping[:, i, j, 0] = ymap
-                self.base_mapping[:, i, j, 1] = xmap
-        self.base_mapping = self.base_mapping.to(self.device)
+        with torch.no_grad():
+            self.base_mapping = prepare_base_mapping(size, phi, theta, fov, batch_size, device)
 
     @torch.no_grad()
     def convert(self, pano_batch: torch.Tensor) -> torch.Tensor:
